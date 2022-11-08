538c05ac79b9095fe48eddf1ee3bbc214f00fb0f
Noel Miño Herrera <noel.minoh@upr.edu.cu>
2022-11-08 16:16:37 -0500

    subidos todos cambios primer video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListadoNoticiasComponent } from './components/listado-noticias/listado-noticias.component';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListadoNoticiasComponent,
+    SpinnerComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule
